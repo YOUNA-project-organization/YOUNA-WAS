de4af117e28a740883d6c371aaf9cb9397085adb
shin donggyu <donggyu9410@gmail.com>
2020-07-26 17:20:20 +0900

    BUILD YOUNA PROJECT CODE FROM TYPESCRIPT TO JAVASCRIPT

--- dist/app.js
+++ dist/app.js
@@ -3,11 +3,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.env = void 0;
 const express_1 = __importDefault(require("express"));
+const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
+const morgan_1 = __importDefault(require("morgan"));
+const morgan_2 = __importDefault(require("morgan"));
+const fs_1 = __importDefault(require("fs"));
+require("./database/mongoose");
+// 현재 서버가 개발 모드인지 프로덕션 모드인지 구분하는 변수를 담는다
+exports.env = (process.env.NODE_ENV && (process.env.NODE_ENV).trim().toLowerCase() == 'production') ? 'production' : 'development';
+const PORT = 5000;
 const app = express_1.default();
-const add = (a, b) => a + b;
-app.get('/', (req, res, next) => {
-    add(5, 5);
-    res.send('Hello');
-});
-app.listen(5000, () => console.log('Server running'));
+app.use(cors_1.default());
+if (exports.env == 'production') {
+    // 배포모드라면 서버에 찍히는 로그들을 access_production.log 라는 이름의 파일에다가 담아주고
+    app.use(morgan_1.default('common', {
+        stream: fs_1.default.createWriteStream('./access_production.log', { flags: 'a' })
+    }));
+}
+else {
+    // 개발모드라면 서버에 찍히는 로그들을 access_development.log 라는 이름의 파일에다가 담아준다
+    app.use(morgan_1.default('common', {
+        stream: fs_1.default.createWriteStream('./access_development.log', { flags: 'a' })
+    }));
+    // 개발모드일때 서버에 찍히는 로그들을 간단하게 터미널에 찍어준다. 
+    app.use(morgan_2.default('dev'));
+}
+app.use('/', routes_1.default);
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
---
+++ dist/controllers/test.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.testController = void 0;
+exports.testController = (req, res) => {
+    res.json({
+        test: true
+    });
+};
