ca80a267ebaf4403678191886605880f7795fc1f
shin donggyu <donggyu9410@gmail.com>
2020-08-02 19:11:04 +0900

    test api done

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -1,7 +1,29 @@
-import { Request, Response } from 'express'
+import { Request, Response, response } from "express";
+import axios from "axios";
+import cheerio from "cheerio";
 
-export const testController = (req: Request, res: Response) => {
+export const testController = async (req: Request, res: Response) => {
+  const encodedUri = encodeURI(
+    `http://store.naver.com/sogum/api/businesses?start=1&display=100&query=공릉동+맛집&sortingOrder=reviewCount`
+  );
+
+  console.log(encodedUri);
+
+  try {
+    const please = await axios.get(encodedUri);
+    // @ts-ignore
+    const datas = please.data.items as [any];
+
+    datas.map((data) => {
+      console.log(data);
+    });
+
     res.json({
-        test: true
-    })
-}
+      ok: true,
+    });
+  } catch (err) {
+    res.json({
+      ok: true,
+    });
+  }
+};
